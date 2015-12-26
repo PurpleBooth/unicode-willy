11eb44dc2b701fb1702c89a68106a854f8bf182a
Billie Thompson <billie@purplebooth.co.uk>
2015-12-26 18:12:22 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Unicode Willy
+=============
+
+It's a web server that prints a ASCII penis. What more do you need?
+
+It's written in go.
---
+++ unicode-willy.go
@@ -0,0 +1,64 @@
+// It's a web server that prints a ASCII penis.
+// Copyright (C) 2015  Billie Thompson <billie@purplebooth.co.uk>
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"html"
+)
+
+const port = 8080
+
+const htmlTemplate = `<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Penis</title>
+</head>
+
+<body>
+  <pre>%s</pre>
+</body>
+</html>`
+
+const penis = `                     _,-%/%|
+                _,-'    \//%\
+            _,-'        \%/|%
+          / / )    __,--  /%\
+          \__/_,-'%(%  ;  %)%
+                  %\%,   %\
+                    '--%'      `
+
+const httpContentTypeValue = "text/html; charset=utf-8"
+const httpContentTypeHeader = "Content-Type"
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Println("8=====>")
+
+	w.Header().Set(httpContentTypeHeader, httpContentTypeValue)
+	fmt.Fprintf(w, htmlTemplate, html.EscapeString(penis))
+}
+
+func main() {
+	log.Println(fmt.Sprintf("Listening on :%d", port))
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
